7216c4a72cd6aa6f0e1941a5e0c9884296451bf3
Allison <snipeslimited@gmail.com>
2017-03-05 23:52:15 -0800

    Initial changes to main.py
    Going through to compare my code with the HW 1&2 solution.

--- main.py
+++ main.py
@@ -1,25 +1,37 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-import webapp2
+import os #loads operating system
+import webapp2 #loads the webapplication
+import jinja2 #does it matter which you import 1st?
 
-class MainHandler(webapp2.RequestHandler):
+import re #what does this mean?
+from string import letters #read up on this
+from google.appengine.ext import db #interpreted as import database from google app engine
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinjia_env = jinjia2.Environment(loader = jinjia2.FileSystemLoader(template_dir),
+                                autoescape = True)
+
+def render_str(self, template, **params): #why was this placed first?
+    t = jinjia_env.get_template(template)
+    return t.render(params)
+
+class BaseHandler(webapp2.RequestHandler):
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))
+
+    def write(self, *a, **kw): #why was order changed for write to render?
+        self.response.out.write(*a, **kw)
+
+class MainPage(Handler):
     def get(self):
-        self.response.write('Hello world!')
+        self.render("Rot13.html")
 
-app = webapp2.WSGIApplication([
-    ('/', MainHandler)
-], debug=True)
+class EncryptHandler(Handler)
+    def get(self):
+        self.render("Rot13.html")
+
+def post(self):
+    rottext = self.request.get("text")
+    rottext = encrypt(rottext)
+    self.render("Rot13.html", rottext = rottext)
+
+app = webapp2.WSGIApplication([('/', MainPage),('/rot13', EncryptHandler)], debug=True)
